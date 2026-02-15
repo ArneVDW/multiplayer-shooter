e9d7942b04035e25f2b24172a18cc18e04ad4c57
ArneVDW <vandewielearne09@gmail.com>
2026-02-15 12:01:34 +0100

    cloudflare tunnel update

--- src/App.jsx
+++ src/App.jsx
@@ -57,7 +57,7 @@ export default function App() {
 
   // Socket initialisatie
   useEffect(() => {
-    const s = io("https://mountains-invision-pace-combining.trycloudflare.com"); // Verbinding met de Pi via de huidige URL
+    const s = io("https://recorded-treatment-plasma-televisions.trycloudflare.com "); // Verbinding met de Pi via de huidige URL
     setSocket(s);
 
     s.on('lobbyUpdate', (data) => {
